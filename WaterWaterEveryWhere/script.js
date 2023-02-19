c6cfa2bef77ee807e529be8c79af11443b295775
garvitmittal124 <garvitmittal124@users.noreply.github.com>
2023-02-19 16:10:44 +0530

    final commit

--- WaterWaterEveryWhere/script.js
+++ WaterWaterEveryWhere/script.js
@@ -238,3 +238,15 @@ window.setInterval(Notification, 5400000)
 a.addEventListener('click', function () {
   chrome.tabs.create({ url: this.href })
 })
+
+function injectTheScript() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    // query the active tab, which will be only one tab
+    //and inject the script in it
+    chrome.tabs.executeScript(tabs[0].id, { file: 'content_script.js' })
+  })
+}
+
+document
+  .getElementById('clickactivity')
+  .addEventListener('click', injectTheScript)
